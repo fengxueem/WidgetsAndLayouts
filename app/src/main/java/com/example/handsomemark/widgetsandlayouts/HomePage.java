281abac15deb8ee7e63b103afbe2fda5d16f43ad
Yinan Fu <fengxueem@gmail.com>
2016-03-14 15:13:26 -0500

    Exercise Linear Layout at 2nd Page

--- app/src/main/java/com/example/handsomemark/widgetsandlayouts/HomePage.java
+++ app/src/main/java/com/example/handsomemark/widgetsandlayouts/HomePage.java
@@ -1,6 +1,7 @@
 package com.example.handsomemark.widgetsandlayouts;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,7 +14,7 @@ import android.widget.ProgressBar;
 
 public class HomePage extends AppCompatActivity {
 
-    private Button b;
+    private Button b,bt2;
     private EditText et;
     private ImageView iv;
 
@@ -23,6 +24,7 @@ public class HomePage extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home_page);
         b = (Button) findViewById(R.id.button);
+        bt2 = (Button) findViewById(R.id.button_goto2);
         et = (EditText) findViewById(R.id.text_edit);
         iv = (ImageView) findViewById(R.id.image_view);
         b.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +37,13 @@ public class HomePage extends AppCompatActivity {
                 p.show();
                 iv.setImageResource(R.drawable.selfiee);
                 et.setText("自拍");
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(HomePage.this,SecondPage.class);
+                startActivity(i);
             }
         });
     }
