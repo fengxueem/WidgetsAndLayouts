011467342df75a1d8581547b9e74542eb4072b70
Yinan Fu <fengxueem@gmail.com>
2016-03-14 14:24:24 -0500

    Add several widgets to HomePage

--- app/src/main/java/com/example/handsomemark/widgetsandlayouts/HomePage.java
+++ app/src/main/java/com/example/handsomemark/widgetsandlayouts/HomePage.java
@@ -1,13 +1,41 @@
 package com.example.handsomemark.widgetsandlayouts;
 
+import android.app.ProgressDialog;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class HomePage extends AppCompatActivity {
+
+    private Button b;
+    private EditText et;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home_page);
+        b = (Button) findViewById(R.id.button);
+        et = (EditText) findViewById(R.id.text_edit);
+        iv = (ImageView) findViewById(R.id.image_view);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProgressDialog p = new ProgressDialog(HomePage.this);
+                p.setTitle("This may take several seconds.");
+                p.setMessage("Loading");
+                p.setCancelable(true);
+                p.show();
+                iv.setImageResource(R.drawable.selfiee);
+                et.setText("自拍");
+            }
+        });
     }
 }
