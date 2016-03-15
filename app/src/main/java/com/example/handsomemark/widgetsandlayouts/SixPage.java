bff8830fe229df34f98ed1c0fa4ba0234664e589
Yinan Fu <fengxueem@gmail.com>
2016-03-15 14:24:20 -0500

    Add list view (fruits) at 6th page

--- app/src/main/java/com/example/handsomemark/widgetsandlayouts/SixPage.java
+++ app/src/main/java/com/example/handsomemark/widgetsandlayouts/SixPage.java
@@ -4,15 +4,23 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by handsomemark on 2016/3/15.
  */
 public class SixPage extends AppCompatActivity {
+
+    private String[] data = {"Apple", "Banana", "Orange", "Watermelon", "Pear", "Avocado", "Kiwi",
+            "Mango", "Cherry", "Strawberry", "Grape", "Pineapple"};
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_six_page);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SixPage.this, android.R.layout.simple_list_item_1,data);
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
     }
 }
